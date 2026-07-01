75be2da58eb12f9d872278461c5f75864f79b72e
JasxnNg <jng@modular.com>
2026-07-01 11:47:38 -0700

    morning commute coding, yay

---
+++ codeforces/2238A.cpp
@@ -0,0 +1,65 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+
+void solve(){
+    int n, c; 
+    std::cin >> n >> c; 
+    std::vector<int>a(n); 
+    std::vector<int>b(n);
+
+    for (auto &x : a){
+        std::cin >> x; 
+    }
+    for (auto &x : b){
+        std::cin >> x;
+    }
+
+    int m1 = 0;
+    for (int i = 0; i < n; i++){
+        if (b[i] > a[i]){
+            m1 = INT32_MAX;
+            break;
+        } else {
+            m1 += (a[i] - b[i]);
+        }
+    }
+
+    int m2 = c; 
+    std::sort(a.begin(), a.end()); 
+    std::sort(b.begin(), b.end());
+
+    for (int i = 0; i < n ; i++){
+        if (b[i] > a[i]){
+            m2 = INT32_MAX;
+            break;
+        } else {
+            m2 += (a[i] - b[i]);
+        }
+    }
+
+    int result = std::min(m1, m2); 
+    if (result == INT32_MAX){
+        std::cout << -1 << '\n'; 
+    } else {
+        std::cout << result << '\n';
+    }
+        
+    
+
+
+
+
+}
+
+int main(){
+    int iterations; 
+    std::cin >> iterations;
+    
+    for (int i = 0; i < iterations; i++){
+        solve();
+    }
+
+
+}
